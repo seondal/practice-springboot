01cb45b8af0efaaba7fac7daf0845f8ca855b3e1
seondal <sseondal@gmail.com>
2023-07-19 15:32:57 +0900

    3.3 Spring Data JPA 테스트코드 작성하기

---
+++ src/test/java/com/org/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.org.example.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "test title";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("sseondal@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
