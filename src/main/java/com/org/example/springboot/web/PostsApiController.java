ba26ac7500f5a8925c9578a2782d89f6e82e1368
seondal <sseondal@gmail.com>
2023-07-19 16:42:22 +0900

    수정 및 조회

--- src/main/java/com/org/example/springboot/web/PostsApiController.java
+++ src/main/java/com/org/example/springboot/web/PostsApiController.java
@@ -1,7 +1,9 @@
 package com.org.example.springboot.web;
 
 import com.org.example.springboot.service.posts.PostsService;
+import com.org.example.springboot.web.dto.PostsResponseDto;
 import com.org.example.springboot.web.dto.PostsSaveRequestDto;
+import com.org.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,5 +15,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
             return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
